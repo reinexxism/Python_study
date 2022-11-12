33d57d65957133b31d98b3285a0331aba1313265
reinexxism <lucy0805@naver.com>
2022-11-12 20:45:37 +0900

    int() 함수 활용하기

---
+++ Chapter02/int_convert.py
@@ -0,0 +1,8 @@
+string_a = input("A를 입력하세요 : ")
+int_a = int(string_a)
+
+string_b = input("B를 입력하세요 : ")
+int_b = int(string_b)
+
+print("문자열 자료 : ", string_a + string_b)
+print("숫자 자료 : ", int_a + int_b)
