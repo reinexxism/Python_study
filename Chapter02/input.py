62ac6f05a371661b87019c62bb359e31fd860931
reinexxism <lucy0805@naver.com>
2022-11-12 20:41:27 +0900

    입력 자료형 확인하기

---
+++ Chapter02/input.py
@@ -0,0 +1,9 @@
+# 입력을 받습니다.
+string = input("입력 : ")
+
+# 출력합니다.
+print("자료 : ", string)
+print("자료형 : ", type(string))
+
+# Python의 Input()의 리턴값의 자료형은 항상 Str(문자열)이다.
+# Input()의 리턴값과 숫자를 연산하려고 하면 오류 발생
