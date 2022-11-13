9ab06a8c64710a5d7e4cf67145703fd89121e67d
reinexxism <lucy0805@naver.com>
2022-11-13 16:00:02 +0900

    리스트에 요소 추가하기

---
+++ Chapter04/list02.py
@@ -0,0 +1,11 @@
+list_a = [1, 2, 3]
+
+print("# 리스트 뒤에 요소 추가하기")
+list_a.append(4) # 리스트명.append(요소)
+list_a.append(5)
+print(list_a)
+print()
+
+print("# 리스트 중간에 요소 추가하기")
+list_a.insert(0, 10) # 리스트명.insert(위치, 요소)
+print(list_a)
