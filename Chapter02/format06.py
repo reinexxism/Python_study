bb1e2ceca0edcaa84eaf8970675312fafc9aafe3
reinexxism <lucy0805@naver.com>
2022-11-13 11:03:57 +0900

    소수점 아래 자릿수 지정하기

--- Chapter02/format06.py
+++ Chapter02/format06.py
@@ -1,6 +1,6 @@
 output_a = "{:15.3f}".format(52.273)
 output_b = "{:15.2f}".format(52.273)
-output_c = "{:15.1f}".format(52.273)
+output_c = "{:15.1f}".format(52.273) # 자동으로 반올림도 수행
 
 print(output_a)
 print(output_b)
