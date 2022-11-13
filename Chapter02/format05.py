e687b27ef0fbd41886fa32c574f43a680f8a7d67
reinexxism <lucy0805@naver.com>
2022-11-13 11:03:35 +0900

    다양한 형태의 부동 소수점 출력하기

--- Chapter02/format05.py
+++ Chapter02/format05.py
@@ -1,7 +1,7 @@
 output_a = "{:f}".format(52.273)
-output_b = "{:15f}".format(52.273)
-output_c = "{:+15f}".format(52.273)
-output_d = "{:+05f}".format(52.273)
+output_b = "{:15f}".format(52.273) # 15칸 만들기
+output_c = "{:+15f}".format(52.273) # 15칸에 부호 추가하기
+output_d = "{:+05f}".format(52.273) # 15칸에 부호 추가하고 0으로 채우기
 
 print(output_a)
 print(output_b)
