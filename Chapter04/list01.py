fb2447478070ba81ced6f984dd0cc14453fe6184
reinexxism <lucy0805@naver.com>
2022-11-13 15:56:45 +0900

    리스트 연산자

--- Chapter04/list01.py
+++ Chapter04/list01.py
@@ -10,8 +10,8 @@ print()
 
 # 기본 연산자
 print("# 기본 연산자")
-print("list_a + list_b =", list_a + list_b)
-print("list_a * 3 =", list_a * 3)
+print("list_a + list_b =", list_a + list_b) # 문자열 연결 연산자 사용
+print("list_a * 3 =", list_a * 3) # 문자열 반복 연산자 사용
 print()
 
 # 함수
