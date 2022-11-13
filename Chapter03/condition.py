7bd4772bedec7ae51850dd33222f586113f042da
reinexxism <lucy0805@naver.com>
2022-11-13 11:12:57 +0900

    조건문의 기본 사용

---
+++ Chapter03/condition.py
@@ -0,0 +1,15 @@
+# Input
+number = input("정수를 입력하세요 : ")
+number = int(number)
+
+# Condition1
+if number > 0 :
+  print("양수입니다.")
+
+# Condition2
+if number < 0 :
+  print("음수입니다.")
+
+# Condition3
+if number == 0 :
+  print("0입니다.")
