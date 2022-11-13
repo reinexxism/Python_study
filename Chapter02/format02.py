3661e42ab3283604d51c7d4ed2dabf5004ce8b18
reinexxism <lucy0805@naver.com>
2022-11-13 10:39:11 +0900

    정수를 특정 칸에 출력하기

--- Chapter02/format02.py
+++ Chapter02/format02.py
@@ -2,12 +2,12 @@
 output_a = "{:d}".format(52)
 
 # 특정 칸에 출력하기
-output_b = "{:5d}".format(52)
-output_c = "{:10d}".format(52)
+output_b = "{:5d}".format(52) # 5칸
+output_c = "{:10d}".format(52) # 10칸
 
 # 빈 칸을 0으로 채우기
-output_d = "{:05d}".format(52)
-output_e = "{:05d}".format(-52)
+output_d = "{:05d}".format(52) # 양수
+output_e = "{:05d}".format(-52) # 음수
 
 # Output
 print("# 기본")
